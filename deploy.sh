f7c4266f37c9253a6db05e5068cbbbb6ba771bbf
신지윤 (Yoon) <108733746+Yoon0221@users.noreply.github.com>
2025-06-21 19:53:14 +0900

    mobileNet 은 다른 계정에서 배포

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 프로젝트 및 서비스 정보
-PROJECT_ID="opensource-project-463412"
+PROJECT_ID="436215849351"
 SERVICE_NAME="streamlit-nutrition-app"
 REGION="asia-northeast3"  # 한국 리전 (서울)
 IMAGE_URI="gcr.io/$PROJECT_ID/$SERVICE_NAME"
